d9eba7bf28e04b1a90706c81533abef126e88d48
Jelil_usman <Mocaretech>
2024-06-20 15:33:04 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc puzzle.c -o puzzle
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("programming is like building a multilingual puzzle");
+	return 0;
+}

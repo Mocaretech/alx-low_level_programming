47075e6463f8bfe547cc78691a7cb69609acce13
Jelil_usman <Mocaretech>
2024-06-24 20:26:27 -0700

    print with exception

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * Print lower case with exception
+ */
+int main(void)
+{
+	char ch = a;
+	while (ch <= 'z')
+	{
+		if (ch !='q' && ch != 'e')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
+		
+
+

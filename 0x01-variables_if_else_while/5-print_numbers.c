8558369ef74befb505cca5ea72767c918951067d
Jelil_usman <Mocaretech>
2024-06-24 21:26:35 -0700

    print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -12,13 +12,13 @@ int main(void)
 {
 	char digit = '0';
 
-	while (digit <= '9')
-   {
+	while(digit <= '9')
+	{
 	   putchar(digit);
 	   digit++;
-    }
+	}
 
 	putchar('\n');
 
-    return (0);
+	return (0);
 }

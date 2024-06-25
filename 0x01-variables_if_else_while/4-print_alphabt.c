c141c1ab220d880e78f3c828233005d02f9a39e0
Jelil_usman <Mocaretech>
2024-06-24 20:43:47 -0700

    prit wit exception

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,15 +10,15 @@
  */
 int main(void)
 {
-    char letter = 'a';
+	char letter = 'a';
 
     while (letter <= 'z')
     {
-        if (letter != 'q' && letter != 'e')
-        {
-            putchar(letter);
-        }
-        letter++;
+	    if (letter != 'q' && letter != 'e')
+	{
+		putchar(letter);
+	}
+	letter++;
     }
 
     putchar('\n');

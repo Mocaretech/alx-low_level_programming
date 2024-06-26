9a1c738e54c5accd302624b5190a9fc8f8867f27
Jelil_usman <Mocaretech>
2024-06-26 07:54:11 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+/**
+ * main point
+ *
+ * Return: always 0
+ */
+int main(){
+	int count = 1;
 
-int main(void)
-{
-    printf("_putchar\n");
-    return 0;
-}
+	while (count < 5){
+		printf("_putchar\n");
+		count++;
+	}
+
+		return 0;
+	}
+

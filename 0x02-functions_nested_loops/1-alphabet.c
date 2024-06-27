b3ad68a9a46e2ae7ddbbbc96cbcde6a11ea95335
Jelil_usman <Mocaretech>
2024-06-27 02:44:43 -0700

    printing alphabet in lower case with _putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,8 +2,10 @@
 #include "main.h"
 
 /**
- *
  * This will print alphabet in lower case followed by a new line
+ *
+ * Description: This function print all lower case from 'a' th 'z'
+ * using _putchar function and then print new line
  */
 void print_alphabet(void)
 {

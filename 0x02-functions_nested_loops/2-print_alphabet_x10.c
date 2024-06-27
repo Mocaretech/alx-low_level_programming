cf0cce06c82156eae31b39c20e6a19a00b30bffa
Jelil_usman <Mocaretech>
2024-06-27 12:35:18 -0700

    print alphabet inlower case

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,7 +12,7 @@ void print_alphabet_x10(void)
 {
 	int i;
 	char letter;
-	
+
 	for (i = 0; i < 10; i++) /* Loop to repeat 10 times */
 	{
 		for (letter = 'a'; letter <= 'z'; letter++) /* Loop through a to z */

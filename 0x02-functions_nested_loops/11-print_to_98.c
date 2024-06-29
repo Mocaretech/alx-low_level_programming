3f9669c566e48517aff31b66c047e930ac8dc1d5
Jelil_usman <Mocaretech>
2024-06-29 15:17:53 -0700

    printing all natural number from 0-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@
  */
 void print_to_98(int n)
 {
-	
+
 	if (n <= 98)
 	{
 		for (; n <= 98; n++)
@@ -21,7 +21,7 @@ void print_to_98(int n)
 				printf("%d\n", n);
 		}
 	}
-	
+
 	else
 	{
 		for (; n >= 98; n--)

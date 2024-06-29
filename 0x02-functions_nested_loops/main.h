a41816127d407507aa312a2dd66411cb16104ccf
Jelil_usman <Mocaretech>
2024-06-28 23:47:22 -0700

    return of sign

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,5 +7,6 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c); /*prototype for _islower */
 int _isalpha(int c); /*prototype for _isalpha */
+int print_sign(int n);
 
 #endif /* MAIN_H */

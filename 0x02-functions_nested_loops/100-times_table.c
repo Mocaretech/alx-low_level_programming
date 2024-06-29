35377e0a75f5c8a0f226c20afeae1a00a3da0a6d
Jelil_usman <Mocaretech>
2024-06-29 15:39:42 -0700

    print 1times table up to 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,10 +13,10 @@ void print_times_table(int n)
 
 {
 	int row, col, product;
-	
+
 	if (n < 0 || n > 15)
 		return;
-	
+
 	for (row = 0; row <= n; row++)
 	{
 		for (col = 0; col <= n; col++)
@@ -26,7 +26,6 @@ void print_times_table(int n)
 			{
 				printf("%d", product);
 			}
-			
 			else
 			{
 				printf(", ");

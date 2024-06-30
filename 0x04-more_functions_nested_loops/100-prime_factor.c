3b7742d3edd3b643b094529f51cb93744c4c6bc0
Jelil_usman <Mocaretech>
2024-06-30 14:45:50 -0700

    printing prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -12,14 +12,14 @@ int main(void)
 	long num = 612852475143;
 	long factor = 2;
 	long largest = 0;
-	
+
 	/* Remove factors of 2 */
 	while (num % factor == 0)
 	{
 		largest = factor;
 		num /= factor;
 	}
-	
+
 	/* Check odd factors from 3 upwards */
 	for (factor = 3; factor <= sqrt(num); factor += 2)
 	{
@@ -29,12 +29,12 @@ int main(void)
 			num /= factor;
 		}
 	}
-	
+
 	/* If num is still greater than 2, then it is prime */
 	if (num > 2)
 		largest = num;
-	
+
 	printf("%ld\n", largest);
-	
+
 	return 0;
 }

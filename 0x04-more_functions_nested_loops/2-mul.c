19e752367874d3b4f10d915a125ffaf6313b502a
Jelil_usman <Mocaretech>
2024-06-30 05:37:06 -0700

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+/* 2-mul.c */
+/**
+ * mul - Multiplies two integers.
+ * @a: The first integer to be multiplied.
+ * @b: The second integer to be multiplied.
+ *
+ * Description: This function takes two integers `a` and `b` as arguments
+ * and returns their product.
+ *
+ * Return: The product of `a` and `b`.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

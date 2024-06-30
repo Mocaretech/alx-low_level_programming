2bff5b1d058255b162380a5f9e5776deecd1fc59
Jelil_usman <Mocaretech>
2024-06-30 14:34:34 -0700

    printing triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,15 +10,15 @@
 void print_triangle(int size)
 {
 	int i, j;
-	
+
 	if (size <= 0)
 	{
 		_putchar('\n');
 		return;
 	}
-	
+
 	for (i = 1; i <= size; i++)
-	
+
 	{
 		for (j = size - i; j > 0; j--)
 		{
@@ -28,7 +28,7 @@ void print_triangle(int size)
 		{
 			_putchar('#');
 		}
-		
+
 		_putchar('\n');
 	}
 }

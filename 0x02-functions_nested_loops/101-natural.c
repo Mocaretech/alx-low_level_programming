793a31472a2d309eba6231d178f18fc191aaf76c
Jelil_usman <Mocaretech>
2024-06-29 22:31:42 -0700

    adding natural number from 0 to 1023

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point of the program.
  *
- * Description: Computes and prints the sum of all multiples of 3 or 5 below 1024.
+ * Description: prints the sum of all multiples of 3 or 5 below 1024.
  *
  * Return: Always 0.
  */
@@ -11,10 +11,10 @@ int main(void)
 {
 	int sum = 0; /* This will hold the sum of multiples*/
 	int i;
-	
+
 	for (i = 0; i < 1024; i++) /* Loop through numbers from 0 to 1023 */
 	{
-		if (i % 3 == 0 || i % 5 == 0) /* Check if the number is a multiple of 3 or 5*/
+		if (i % 3 == 0 || i % 5 == 0) /* if the number is multiple of 3 or 5*/
 		{
 			sum += i; /* Add the number to the sum*/
 		}

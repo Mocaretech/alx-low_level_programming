59ed1f0fdd70772541abfd3bf75656a65b28eabf
Jelil_usman <Mocaretech>
2024-06-30 13:37:32 -0700

    printing size

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+/* 8-print_square.c*/
+#include "main.h"
+
+/**
+ * print_square - Prints a square using the character #.
+ * @size: The size of the square.
+ *
+ * Description: This function prints a square with the character #
+ * of dimensions size x size. If size is 0 or less, it only prints a new line.
+ */
+void print_square(int size)
+{
+	int i, j;
+	
+	if (size <= 0)
+	{
+		_putchar('\n'); /* Print a newline if size is 0 or less */
+	}
+	else
+	
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#'); /* Print the # character */
+			}
+			_putchar('\n'); /* Move to the next line */
+		}
+	}
+}

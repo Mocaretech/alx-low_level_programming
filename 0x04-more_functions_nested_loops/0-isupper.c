2bd51c6b18a5fb3921b3397ee9775409c6f54233
Jelil_usman <Mocaretech>
2024-06-30 05:14:23 -0700

    printing uppercase letter

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,12 @@
 /*0-isupper.c*/
-/*
+/**
  * _isupper - Checks if a character is uppercase.
  * @c: The character to be checked.
+ *
+ * Description: This function checks if the provided character `c`
+ * is an uppercase letter. Uppercase letters are those in the range
+ * 'A' to 'Z'. If the character is uppercase, the function returns 1.
+ * Otherwise, it returns 0.
  *
  * Return: 1 if character is uppercase, 0 otherwise.
  */

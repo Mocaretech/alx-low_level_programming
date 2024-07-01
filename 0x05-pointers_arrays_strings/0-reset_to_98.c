ecfd63e4df9a76846e6fe82595de7328a649052a
Jelil_usman <Mocaretech>
2024-07-01 12:42:10 -0700

    reseting pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/*0-reset_to_98.c*/
+#include "main.h"
+
+/**
+ * Reset_to_98 - Updates the value of the integer to 98
+ * @n: Apointer to an interger to be updated
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

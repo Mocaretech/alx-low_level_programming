730f9f65a93f55637eddc8732760518b07a22916
Jelil_usman <Mocaretech>
2024-07-01 13:51:19 -0700

    declare pointer a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,25 @@
+/*2-strlen.c*/
+#include "main.h"
+
+/**
+ *_strlen - Returns the length of a string.
+ *@s: A pointer to a string (character array).
+ *
+ *Return: The length of the string.
+ *
+ *This function iterates through the characters in the string until
+ *it encounters the null terminator '\0' and counts the number of
+ *characters in the string.
+*/
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	return length;
+}

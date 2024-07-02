f570c6c0866df13f83dc7228a40f60bd5c6d6587
Jelil_usman <Mocaretech>
2024-07-01 23:37:21 -0700

    function that prints n elements of an array of integers, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *  * _strcpy - Copies the string pointed to by src, including the terminating
+ *   * null byte (\0), to the buffer pointed to by dest.
+ *    * @dest: A pointer to the destination buffer.
+ *     * @src: A pointer to the source string.
+ *      *
+ *       * Return: The pointer to dest.
+ */
+char *_strcpy(char *dest, const char *src)
+{
+	char *ptr = dest; /* Save the start of the destination pointer */
+
+	/* Iterate over each character in the source string */
+
+	while (*src != '\0')
+
+	{
+		*dest = *src; /* Copy the character from src to dest */
+		dest++;
+		src++;
+	}
+
+	*dest = '\0'; /* Copy the null terminator to dest */
+
+	return ptr; /* Return the pointer to the start of dest */
+}

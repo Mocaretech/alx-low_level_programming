6132f694565b7c52d45976abb33dbc3d46fef53c
Jelil_usman <Mocaretech>
2024-07-04 20:55:06 -0700

    function that prints a buffer.

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,49 @@
+/*104-print_buffer.c*/
+#include <stdio.h>
+#include <ctype.h>
+
+void print_buffer(char *b, int size)
+{
+	int offset, i;
+
+	if (size <= 0)
+
+	{
+		printf("\n");
+		
+		return;
+	}
+
+	for (offset = 0; offset < size; offset += 10)
+	
+	{
+		/* Print the offset*/
+
+		printf("%08x: ", offset);
+		/* Print the hex content, 2 bytes at a time*/
+		for (i = 0; i < 10; i++)
+		{
+			if (offset + i < size)
+				printf("%02x", (unsigned char)b[offset + i]);
+			else
+				printf("  ");
+			if (i % 2 != 0)
+				printf(" ");
+		}
+
+		/* Print the ASCII content*/
+
+		for (i = 0; i < 10; i++)
+		{
+			if (offset + i < size)
+			{
+				char c = b[offset + i];
+				if (isprint(c))
+					putchar(c);
+				else
+					putchar('.');
+			}
+		}
+		printf("\n");
+	}
+}

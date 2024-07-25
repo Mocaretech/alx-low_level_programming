d02655869694c15763da2c2ced32e163fa56f184
Jelil_usman <Mocaretech>
2024-07-24 21:08:57 -0700

    This program will run an operation of sum,multiplication

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -37,13 +37,6 @@ int op_mul(int a, int b)
 	return (a * b);
 }
 
-/**
- * op_div - Returns the result of the division of a by b.
- * @a: The first integer.
- * @b: The second integer.
- *
- * Return: The result of the division of a by b.
- */
 int op_div(int a, int b)
 {
 	if (b == 0)
@@ -53,14 +46,6 @@ int op_div(int a, int b)
 	}
 	return (a / b);
 }
-
-/**
- * op_mod - Returns the remainder of the division of a by b.
- * @a: The first integer.
- * @b: The second integer.
- *
- * Return: The remainder of the division of a by b.
- */
 int op_mod(int a, int b)
 {
 	if (b == 0)

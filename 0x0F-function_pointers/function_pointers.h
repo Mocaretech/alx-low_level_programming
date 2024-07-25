5572714ac2133e320e48cc03dcbb660524a7bc1c
Jelil_usman <Mocaretech>
2024-07-24 19:56:12 -0700

    function that searches for an integer.

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -9,5 +9,6 @@ void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int _putchar(char c);
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /* FUNCTION_POINTERS_H */
